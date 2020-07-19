a359243745941128697745686ca09ad2658008d9
Arthur Barbosa <arthurbruno03@gmail.com>
2020-07-19 17:37:36 -0400

    create project

---
+++ src/main/java/com/abcode/multiconnectiondata/multiconnectiondata/MulticonnectiondataApplication.java
@@ -0,0 +1,13 @@
+package com.abcode.multiconnectiondata.multiconnectiondata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulticonnectiondataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MulticonnectiondataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abcode/multiconnectiondata/multiconnectiondata/MulticonnectiondataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcode.multiconnectiondata.multiconnectiondata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulticonnectiondataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
