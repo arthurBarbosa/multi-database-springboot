3cccb8889a4b2771dc6d829e0d29712fd99eb626
Arthur Barbosa <arthurbruno03@gmail.com>
2020-07-19 19:27:47 -0400

    config initial database

---
+++ src/main/java/com/abcode/multiconnectiondata/multiconnectiondata/config/AppDbConfig.java
@@ -0,0 +1,38 @@
+package com.abcode.multiconnectiondata.multiconnectiondata.config;
+
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "appEntityManager")
+public class AppDbConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "app.datasource")
+    public DataSource appDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean appEntityManager(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("appDataSource") DataSource dataSource) {
+        return builder.dataSource(dataSource)
+                .packages("com.abcode.multiconnectiondata.domain.app")
+                .build();
+    }
+
+}
